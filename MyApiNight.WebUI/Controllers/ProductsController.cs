231811a542408cadd7dcb375818044b6cf1b3adc
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-12-19 20:47:11 +0300

    Add project files.

--- MyApiNight.WebUI/Controllers/ProductsController.cs
+++ MyApiNight.WebUI/Controllers/ProductsController.cs
@@ -16,11 +16,11 @@ namespace MyApiNight.WebUI.Controllers
 
         public async Task<IActionResult> ProductList()
         {
-            var client=_httpClientFactory.CreateClient();
+            var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7292/api/Product");
             if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 return View(values);
             }
@@ -36,9 +36,9 @@ namespace MyApiNight.WebUI.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
         {
-            var client=_httpClientFactory.CreateClient();
-            var jsonData=JsonConvert.SerializeObject(createProductDto);
-            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PostAsync("https://localhost:7292/api/Product", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
@@ -47,5 +47,44 @@ namespace MyApiNight.WebUI.Controllers
             return View();
         }
 
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7292/api/Product?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7292/api/Product/GetProduct?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7292/api/Product", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
     }
 }
---
+++ MyApiNight.WebUI/Dtos/UpdateProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MyApiNight.WebUI.Dtos
+{
+    public class UpdateProductDto
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public decimal ProductPrice { get; set; }
+    }
+}
